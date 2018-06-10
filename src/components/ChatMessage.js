d46cc6ca489ada88f536e8288edb90ebb0281602
LinusBF <linus.webdevelopment@gmail.com>
2018-06-10 09:15:56 +0200

    Added timezone handling to messages before displaying to user

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -7,10 +7,14 @@ import React from 'react';
 export default ChatMessage;
 
 function ChatMessage(props) {
+    var dateCorrected = new Date(props.messageData.date);
+    var timeZoneCorrected = dateCorrected.getTime() - dateCorrected.getTimezoneOffset()*(1000*60);
+    dateCorrected.setTime(timeZoneCorrected);
+
     return(
         <div className="chat-message">
             <div className="message-time">
-                { props.messageData.date.toISOString().slice(11,19) }:
+                { dateCorrected.toISOString().slice(11,19) }:
             </div>
             <div className={(props.messageData.userContent ? "user-input" : "bot-message") + " message-text"}>
                 { props.messageData.message }
